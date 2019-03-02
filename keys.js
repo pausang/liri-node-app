61937b8760470d8bec50c5708526649c01d2ea98
pausang <kokosangpi@gmail.com>
2019-03-01 23:25:19 -0600

    final edit and commit

--- keys.js
+++ keys.js
@@ -1,25 +1,47 @@
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.SPOTIFY_SECRET
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
 
-var fs = require ('fs');
-var axios = require('axios');
+// var fs = require ('fs');
+// var axios = require('axios');
 
 
-var keys = function() {
-    this.findShow = function(show) {
-        var URL ="http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6" + show;
-        axios
-        .get(URL)
-        .then(function(response) {
-            fs.appendFile("log.txt", JSON.stringify(response.data),function (err){
-                if (err) {
-                    console.log(err);
-                }
-            })
-            console.log(response.data.)
-        })
-    }
-}
+// var spotify = function() {
+//     this.findShow = function(show) {
+//         var URL ="https://api.spotify.com/v1/browse/new-releases";
+//         axios
+//         .get(URL)
+//         .then(function(response) {
+//             fs.appendFile("log.txt", JSON.stringify(response.data),function (err){
+//                 if (err) {
+//                      console.log(err);
+//                 }
+//             })
+//             console.log(response.data.name);
+//             console.log(response.data.genres);
+//             console.log(response.data.rating.average);
+//             console.log(response.data.network.name);
+//             console.log(response.data.sumary);
+//         })
+//     };
+//     this.findArtists = function(artists) {
+//         var URL = "https://api.spotify.com/v1/browse/new-releases?country=SE" + artists;
+//         axios.get(URL)
+//         .then(function(response) {
+//             fs.appendFile("log.txt", JSON.stringify(response.data), function (err) {
+//                 if (err) {
+//                     console.log(err.response);
+//                 }
+//             })
+//             console.log(response.data.name);
+//             console.log(response.data.genres);
+//             console.log(response.data.rating.average);
+//             console.log(response.data.network.name);
+//             console.log(response.data.summary);
+//         })
+//     }
+// };
+// module.exports = spotify;
--- liri.js
+++ liri.js
@@ -1,28 +1,74 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var fs = require ('fs');
+var axios = require('axios');
 
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify)
 //console.log(spotify);
 
-var title = process.argv[2];
+function concertThis(artist) {
 
-var name = process.argv.slice(3).join(" ");
-
-if (!title) {
-    title = "show";
-}
-
-if (!name) {
-    name = "song";
-}
-
-if (title === "show") {
-    console.log("Searching for Spotify show");
+    axios.get("https://rest.bandsintown.com/artists/" + artist + " /events?app_id=codingbootcamp").then(function(
+    response){
     
-} else {
-    console.log("Searching for Spotify Song");
+        console.log("Concert-this:" + response.data.name);
+        console.log('');
+        console.log('===========================');
+        console.log('result:'+ ' ' + venue.name);
+    }
+);
 
 }
 
-console.log(title + " " + name);
+
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+
+      
+       axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+           function(response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+            console.log("Title of the movie:" + response.data.title);
+            console.log("Year the movie came out: " + response.data.year);
+            console.log("Rotten Tomatoes Rating of the move " + response.data.tomatoesRating);
+            console.log("Language of the movie " + response.any.language);
+            console.log("Plot of the movie" + response.data.plot);
+            console.log("Actors in the movie " + response.data.actors);
+           }
+       );
+         
+      
+       function logBefore(func, message) {
+        console.log(message);
+      
+        func();
+      }
+      
+      function runIf(func, bool) {
+        if (bool) {
+          func();
+        }
+      }
+      
+      function wrap(func, value) {
+        return function() {
+          return func(value);
+        };
+      }
+      fs.writeFile("random.txt", "I Want it That Way!", function(err) {
+
+        if (err) {
+          console.log(err);
+        }
+      
+        console.log("File saved!");
+      
+      });
+            
